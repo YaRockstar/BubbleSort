31c60562eb586e224c89c70aa237ef90c03e0eac
ya_rockstar <yekalashnikov@gmail.com>
2023-08-13 00:26:09 +0300

    add: add output to the console of the array before sorting

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 import java.util.Arrays;
 
 public class Main {
-    /**
-     * Пузырьковая сортировка (Сортировка пузырбком, Bubble sort).
-     * Особенности:
-     * - "In-place" алгоритм - алгоритм, который использует малое кол-во памяти (не зависит от N).
-     * - Stable (стабильный, устойчивый), то есть сохраняет относительный порядок элементов с одинаковыми значениями.
-     * - Временная сложность квадратичаня O(N^2).
-     * - Быстро деградирует, то есть время работы растет нелинейно.
-     */
     public static void swap(int[] arr, int i, int j) {
         if (i == j) {
             return;
         }
+
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -31,8 +24,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] array = {4, 2, 1, 9, 0, 7, 6, 5, 8, 3};
+        int[] array = {4, 2, 1, 9, 0, 1, 7, 6, 3, 5, 8, 3};
+        System.out.println("Before: " + Arrays.toString(array));
         bubbleSort(array);
-        System.out.println(Arrays.toString(array));
+        System.out.println("After: " + Arrays.toString(array));
     }
 }
